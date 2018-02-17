dce70b7f823849d5ff22bdef0f442c8ab87a1a9d
Ruben Ortiz <rubenortizg@gmail.com>
2018-02-16 20:43:46 -0500

    Init MVC

---
+++ vistas/modulos/footer.php
@@ -0,0 +1,4 @@
+<footer class="main-footer">
+  <strong>Copyright &copy; 2018, <a href="http://www.ruddor.com.co" target="_blank">Ruddor - Consultoría Tecnológica</a>.</strong>
+  Todos los derechos reservados.
+</footer>
---
+++ vistas/modulos/menu.php
@@ -0,0 +1,68 @@
+<aside class="main-sidebar">
+  <section class="sidebar">
+    <ul class="sidebar-menu">
+      <li class="active">
+        <a href="inicio">
+          <i class="fa fa-home"></i>
+          <span>Inicio</span>
+        </a>
+      </li>
+
+      <li>
+        <a href="usuarios">
+          <i class="fa fa-user"></i>
+          <span>Usuarios</span>
+        </a>
+      </li>
+
+      <li>
+        <a href="clientes">
+          <i class="fa fa-users"></i>
+          <span>Clientes</span>
+        </a>
+      </li>
+
+      <li>
+        <a href="inmuebles">
+          <i class="fa fa-building"></i>
+          <span>Inmuebles</span>
+        </a>
+      </li>
+
+      <li class="treeview">
+        <a href="#">
+          <i class="fa fa-credit-card"></i>
+          <span>Recibos</span>
+          <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+          </span>
+        </a>
+
+        <ul class="treeview-menu">
+          <li>
+            <a href="comprobantes">
+              <i class="fa fa-circle-o"></i>
+              <span>Comprobantes pago</span>
+            </a>
+          </li>
+
+          <li>
+            <a href="ingresos">
+              <i class="fa fa-arrow-left"></i>
+              <span>Ingresos</span>
+            </a>
+          </li>
+
+          <li>
+            <a href="egresos">
+              <i class="fa fa-arrow-right"></i>
+              <span>Egresos</span>
+            </a>
+          </li>
+
+        </ul>
+      </li>
+
+    </ul>
+  </section>
+</aside>
